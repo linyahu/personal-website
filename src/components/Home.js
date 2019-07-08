8eee43383f64a0025bb9899c82f31e69170eea34
github username <github email address>
2019-07-08 10:36:46 -0400

    testing canvas

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,23 @@ import React, { Component } from 'react'
 class Home extends Component {
   // render a cool animation with canvas
 
+  componentDidMount(){
+    this.drawSomething()
+  }
+
+  drawSomething() {
+    let canvas = document.querySelector("#canvas")
+    let c = canvas.getContext("2d")
+
+    c.fillStyle = ("rgb(255,255,255)")
+    c.fillRect(350,350,50,50)
+  }
+
   render() {
     return (
-      <div className="outer-container">
-        <h1>home page</h1>
+      <div>
+        <canvas id="canvas" width="500" height="500"></canvas>
+
       </div>
     )
   }
